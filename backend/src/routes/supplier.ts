30e4ea315da3097d274391c9b3cb76d524e15302
ryanestoque <estoque23atsup@gmail.com>
2025-12-10 00:05:44 +0800

    Updated routes

--- backend/src/routes/supplier.ts
+++ backend/src/routes/supplier.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addSupplier, deleteSupplier, getAllSuppliers, updateSupplier } from "../controllers/supplierController";
+import { addSupplier, deleteSupplier, getAllActiveSuppliers, getAllSuppliers, updateSupplier } from "../controllers/supplierController";
 
 const router = Router();
 
 router.get("/", getAllSuppliers);
+router.get("/active", getAllActiveSuppliers);
 router.post("/", addSupplier);
 router.put("/:supplier_id", updateSupplier);
 router.delete("/:supplier_id", deleteSupplier);
