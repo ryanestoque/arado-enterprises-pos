903952ab3c401ace4bce07e75287c08dc67b82f2
ryanestoque <estoque23atsup@gmail.com>
2025-12-13 10:38:05 +0800

    updated db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -42,6 +42,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 export default db;
